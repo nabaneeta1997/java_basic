334e683a9e184cbe97ef0f111d54adbb6e177ad8
nabaneeta1997 <ashokenaba@gmail.com>
2019-09-01 22:14:04 +0530

    student

--- java_basics/src/java_basics/Java_basics.java
+++ java_basics/src/java_basics/Java_basics.java
@@ -18,7 +18,17 @@ public class Java_basics {
         // TODO code application logic here
         System.out.println("done");
         System.out.println("tax nbo ami bhujle");
-        /* ekta line add krlm then eibar dekho magic */
+        /* 
+            ekta line add krlm then eibar dekho magic 
+            20 and 21 line ami add krechi and tumi github desktop e jei file
+            glo tulte chao sei glo check krbe ar jei glo tulte chao na github e
+            sei glo uncheck krbe.
+        
+            file jei glo tulbe sei glo check korar por tmke ekta comment likhte hbe
+            better hbe ki kaj krecho seita short cut e likhe commit kora uchit.
+            busss ajke class seshlo.
+            then tmke push origin bolbe click krbe tate then tmr code github e add hoy jbe
+        */
     }
     
 }
